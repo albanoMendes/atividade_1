5904f37b1357dfde1149c92c63764da0bfe11085
BrunoNoGithub <brunopamplona10@hotmail.com>
2022-05-23 19:46:30 -0300

    Remoção de erroas e melhoria de legibilidade

--- hierholzeher.py
+++ hierholzeher.py
@@ -3,24 +3,25 @@ import sys
 from grafo import Grafo
 import random
 
+def existeVizinhoNaoVisitado(grafo, v,C):
+    vizinhosv=grafo.vizinhos(v)
+    for vizinho in vizinhosv:
+        for ligacao in C:
+            if ligacao==({v,vizinho},False):
+                return True
+    return False
+
+
 def buscarSubcicloEuleriano(grafo, v,C):
     ciclo=[v]
     t=v
-    vizinhosv=grafo.vizinhos(v)
     vdiferenteT=True
-    while v!=t:
-        for vizinho in vizinhosv:
-            for ligacao in C:
-                if ligacao==({v,vizinho},False):
-                    ligacao=({v,vizinho},False)
-                    v=ligacao
-                    ciclo.append(v)
-                    if v==t:
-                        vdiferenteT=False
-
+    while vdiferenteT==True:
+        if existeVizinhoNaoVisitado(grafo,v,C)==False:
+            return False
+        else:
+            pass #continuar da linha 7 do subciclo
     
-    
-    return (False,[0])
         
 
 
@@ -28,32 +29,26 @@ grafo = Grafo(direcionado=False, ponderado=False)
 grafo.ler(sys.argv[1])  # Lê o arquivo de grafo passado como argumento.
 #s = int(sys.argv[2])  # Define o vértice de origem passado como argumento.
 
-#   LINHAS 1 A 2 DO CICLO
+#   LINHAS 1 A 2 DO ALGORITMO
 C = []
 for v in range(0,grafo.qtdVertices()):
     for v2 in range(0,grafo.qtdVertices()):
       if grafo.haAresta(v, v2):
        C.append({v, v2}, False)
 
-
+#v<-Vértice aleatório de V
 n = random.randint(0,grafo.qtdVertices())
-v = grafo.vertices[n];
+v = grafo.vertices[n]
 (r, ciclo) = buscarSubcicloEuleriano(grafo, v,C)
 
 
-e=False
-
-if r==False:
-    print(0)
-
-
-else:
-    e=True
+existeCiclo=0
+#Se método chamado retornar falso, imprimir 0, não tem ciclo euleriano
+if r==True: #caso contrario
     for aresta in C:
         if aresta[1]==False:
-            e=False
-            print(0)
-            
-if e==True:
-    print(1)
+            existeCiclo=1           
+            break
+print(existeCiclo)
+if existeCiclo==1:
     print(ciclo)
